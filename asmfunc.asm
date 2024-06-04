8ae23b80b4bba0cc5ce1a81e685cc710cd1a4f77
Pepitorino <carlos.ambrosio2007@gmail.com>
2024-06-04 10:42:06 +0800

    avx2 started

--- asmfunc.asm
+++ asmfunc.asm
@@ -35,9 +35,31 @@ L1:
 xmm_1D_stencil:
 
 	xor rax, rax
+	ret
 
 
 ymm_1D_stencil:
+	sub rcx, 0x0000_0000_0000_0111
+	xor rsi, rsi
+	ymmloop1:
+		vmovdqu ymm0, [rdx+rsi*4]
+		vmovdqu ymm1, [rdx+rsi*4+4]
+		vmovdqu ymm2, [rdx+rsi*4+8]
+		vmovdqu ymm3, [rdx+rsi*4+12]
+		vmovdqu ymm4, [rdx+rsi*4+16]
+		vmovdqu ymm5, [rdx+rsi*4+20]
+		vmovdqu ymm6, [rdx+rsi*4+24]
+		vpaddd ymm7, ymm0, ymm1
+		vpaddd ymm8, ymm2, ymm3
+		vpaddd ymm9, ymm4,ymm5
+		vpaddd ymm10, ymm7, ymm6
+		vpaddd ymm11, ymm8, ymm9
+		vpaddd ymm12, ymm10, ymm11
+		vmovdqu [R8+rsi*4], ymm12
+		add rsi, 0x0000_0000_0000_1000
+		sub rcx, 0x0000_0000_0000_1000
+		cmp rcx, 0x0
+		jg ymmloop1
 
 	xor rax, rax
 	ret
--- main.c
+++ main.c
@@ -42,7 +42,7 @@ int main() {
 	for (i = 0; i < Y_ARRAY_SIZE; i++) 
 		y[i] = 0;
 	
-		
+	ymm_1D_stencil(Y_ARRAY_SIZE, x, y);
 
 	//C function 
 	start = clock();
@@ -104,7 +104,32 @@ int main() {
 
 
 	//x86 SIMD AVX2 ASM function using YMM
+	start = clock();
+	ymm_1D_stencil(Y_ARRAY_SIZE, x, y); //call function
+	end = clock();
+	time_taken = (double)(end - start) * 1e3 / CLOCKS_PER_SEC;
+	printf("First run initialization. Time in x86-64 AVX2: %lf ms\n", time_taken);
 
+	time_total = 0.0;
+	for (i = 0; i < loopcount; i++) {
+		start = clock();
+		ymm_1D_stencil(Y_ARRAY_SIZE, x, y); //call function 
+		end = clock();
+		time_taken = (double)(end - start) * 1e3 / CLOCKS_PER_SEC;
+		printf("Run #%d. Time in x86-64 AVX2: %lf ms\n", i, time_taken);
+		time_total += time_taken;
+	}
+	printf("Output Y: \n");
+	printf("First 10 elements: ");
+	for (int j = 0; j < 10; j++)
+		printf("%d ", y[j]);
+	printf("\nLast 10 elements: ");
+	for (int j = Y_ARRAY_SIZE - 10; j < Y_ARRAY_SIZE; j++)
+		printf("%d ", y[j]);
+
+	printf("\n");
+	time_average = time_total / loopcount;
+	printf("Total time taken in x86-64 AVX2: %lf ms.\nAverage x86-64 AVX2 runtime across 30 executions: %lf ms.\n", time_total, time_average);
 
 	free(x);
 	free(y);
